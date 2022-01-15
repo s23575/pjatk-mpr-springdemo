06f153961d94d47b938abe179c5d9526f0fecc6f
s23575 <s23575@pjwstk.edu.pl>
2022-01-15 20:51:55 +0100

    Projekt semestralny

---
+++ springdemo/src/main/java/pl/edu/pjatk/mpr/springdemo/SpringdemoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.mpr.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoApplication.class, args);
+	}
+
+}
---
+++ springdemo/src/test/java/pl/edu/pjatk/mpr/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.mpr.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
