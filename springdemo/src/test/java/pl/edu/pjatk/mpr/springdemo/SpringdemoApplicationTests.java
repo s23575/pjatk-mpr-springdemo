7482057917592dc8a77815457b1bbace38ab0ce4
s23575 <s23575@pjwstk.edu.pl>
2022-01-19 12:56:42 +0100

    Projekt semestralny

--- springdemo/src/test/java/pl/edu/pjatk/mpr/springdemo/SpringdemoApplicationTests.java
+++ springdemo/src/test/java/pl/edu/pjatk/mpr/springdemo/SpringdemoApplicationTests.java
@@ -3,12 +3,12 @@ package pl.edu.pjatk.mpr.springdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Domyślny test (integracyjny) w aplikacji springowej - sprawdza, czy aplikacja się uruchamia (start i stop,
-// natychmiast)
+// * Domyślny test (integracyjny) w aplikacji springowej – sprawdza, czy aplikacja się uruchamia (start i stop,
+//   natychmiast); najważniejsza część testu integracyjnego to załadowanie kontekstu – to decyduje, że coś jest
+//   testem integracyjnym.
 
-// Najważniejsza część testu integracyjnego - ładuje kontekst; to decyduje o tym, że coś jest testem integracyjnym
 @SpringBootTest
-class SpringdemoApplicationTests {
+class SpringdemoApplicationTests {		// *
 
 	@Test
 	void contextLoads() {
